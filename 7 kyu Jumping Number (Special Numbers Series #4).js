cf24ef1fd2b9cb99554e846bf7adab1cc914d51b
vladimirtonkov <62721503+vladimirtonkov@users.noreply.github.com>
2021-07-07 17:46:12 +0400

    7 kyu Jumping Number

---
+++ 7 kyu Jumping Number (Special Numbers Series #4).js
@@ -0,0 +1,25 @@
+//
+
+function jumpingNumber(n){
+    if (0 <= n && n <= 9) {
+    return "Jumping!!";
+  }
+
+  let count = 0;
+  let num = String(n).split("");
+  for (let i = 0; i < num.length; i++) {
+    if (i + 1 < num.length) {
+      let a = num[i] - num[i + 1];
+      let b = num[i + 1] - num[i];
+      if ((a === 1) | (b === 1)) {
+        count++;
+      }
+    }
+  }
+  return num.length - count === 1 ? "Jumping!!" : "Not!!";
+}
+
+
+// Examples
+// jumpingNumber(556847) ==> return "Not!!"
+// Adjacent digits don't differ by 1
